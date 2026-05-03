c7d44d6979ed461fb2ec9434c77248716c6809d7
ArefOrumiehei <areforumiehei@gmail.com>
2026-05-03 23:27:36 +0330

    🌺 Change Background Color

--- src/layouts/mainLayout/MainLayout.tsx
+++ src/layouts/mainLayout/MainLayout.tsx
@@ -7,7 +7,7 @@ function MainLayout() {
   const isProfile = pathname.startsWith("/profile");
 
   return (
-    <div className="flex flex-col min-h-screen pb-18 md:pb-0 bg-sky-100">
+    <div className="flex flex-col min-h-screen pb-18 md:pb-0 bg-gray-50/50">
       <Header />
       <main className="flex-1 w-full px-4 md:px-[3rem]">
         <Outlet />
