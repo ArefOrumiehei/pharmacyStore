66c4b9e9dff23c81d7d33a1d396549f196886dd8
ArefOrumiehei <areforumiehei@gmail.com>
2026-05-15 02:29:17 +0330

    🌼 Add a helper for formating text numbers to FA

--- src/helpers/formaters.ts
+++ src/helpers/formaters.ts
@@ -1,1 +1,4 @@
 export const formatNumberToFa = (n: number | string) => new Intl.NumberFormat("fa-IR").format(Number(n));
+
+export const toPersianNums = (str: string | number): string =>
+  String(str).replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[+d]);
