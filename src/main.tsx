a61882bc51e55317191bdd04ee085fb14ccc2dc8
ArefOrumiehei <areforumiehei@gmail.com>
2026-07-31 23:10:32 +0330

    🌸 Wrap application in QueryClientProvider for react-query integration

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,31 @@ import { BrowserRouter } from "react-router";
 import { ToastContainer } from 'react-toastify';
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <ToastContainer 
-        theme='colored' 
-        rtl 
-        position='top-left' 
-        className={"toast-container"} 
-        autoClose={3000}
-      />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer 
+          theme='colored' 
+          rtl 
+          position='top-left' 
+          className={"toast-container"} 
+          autoClose={3000}
+          />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
