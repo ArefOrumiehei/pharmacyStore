4ed6e9ff73e15b242f5ce0cc256d3612d58c5b74
ArefOrumiehei <areforumiehei@gmail.com>
2026-08-13 13:22:24 +0330

    ♻️ refactor: return res.data instead of res.data.data

--- src/services/commentServices/commentServices.ts
+++ src/services/commentServices/commentServices.ts
@@ -9,20 +9,20 @@ export const addComment = async (props: AddComemnt) => {
 
 export const editComment = async (id: number, message: string, rate: number) => {
   const res = await apiInstance.put("/api/Comment/edit", {id, message, rate})
-  return res.data.data;
+  return res.data;
 }
 
 export const deleteComment = async (commentId: number) => {
   const res = await apiInstance.delete(`/api/Comment/delete/${commentId}`)
-  return res.data.data;
+  return res.data;
 }
 
 export const likeComment = async (commentId: number) => {
   const res = await apiInstance.post(`/api/Comment/like/${commentId}`)
-  return res.data.data;
+  return res.data;
 }
 
 export const dislikeComment = async (commentId: number) => {
   const res = await apiInstance.post(`/api/Comment/dislike/${commentId}`)
-  return res.data.data;
+  return res.data;
 }
